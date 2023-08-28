1fe548daed07f687574613f2e48c3f50129f8a85
Del <wjg12341234@163.com>
2023-08-28 23:57:31 +0800

    fix: fetch ens, nfts and tokens (from metamask)

--- src/pages/core/index.ts
+++ src/pages/core/index.ts
@@ -14,42 +14,67 @@ export const core = {
   async getToken(
     _address?: string,
   ): Promise<{ total: string; tokens: Token[] }> {
+    const _tokens: any[] = [];
     const address = _address ?? core.deafultAddress;
-    const datas = await http.proxy.get<any[]>(
-      `https://openapi.debank.com/v1/user/token_list?is_all=false&id=${address}`,
-      undefined,
-      {
-        headers: {
-          Referer: 'https://fans3.5degrees.io/',
+    for (const chain of [1, 56, 137]) {
+      const datas = await http.proxy.get(
+        `https://account.metafi.codefi.network/accounts/${address}/?includePrices=true&chainId=${chain}`,
+        undefined,
+        {
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/116.0',
+            Origin: 'https://portfolio.metamask.io',
+            Referer: 'https://portfolio.metamask.io/',
+            'Cache-Control': 'no-cache',
+          },
         },
-      },
-    );
+      );
+      const chainIdMaps = {
+        1: 'eth',
+        56: 'bsc',
+        137: 'matic',
+      };
+      if (datas?.nativeBalance) {
+        const e = datas?.nativeBalance;
+        _tokens.push({
+          name: e.name,
+          symbol: e.symbol,
+          logo: e.iconUrl,
+          chainId: e.chainId,
+          chain:
+            chainsMap[chainIdMaps[e.chainId] ?? -1]?.logo ?? chainsMap.eth.logo,
+          amount: e.balance,
+          value: e.value.marketValue,
+        });
+      }
+      for (const e of datas?.tokenBalances ?? []) {
+        _tokens.push({
+          name: e.name,
+          symbol: e.symbol,
+          logo: e.iconUrl,
+          chain:
+            chainsMap[chainIdMaps[e.chainId] ?? -1]?.logo ?? chainsMap.eth.logo,
+          amount: e.balance,
+          value: e.value.marketValue,
+        });
+      }
+    }
     // 只取价值大于 $1 的币
-    const coins = (isArray(datas) ? datas! : []).filter((e) => {
-      return e.amount * e.price > 1;
-    });
+    const coins = _tokens.filter((e) => e.value > 1);
     // 总价值
-    const total = formatNumber(
-      coins.reduce((pre, e) => {
-        return pre + e.amount * e.price;
-      }, 0),
-    );
+    const total = formatNumber(coins.reduce((pre, e) => pre + e.value, 0));
     const tokens = coins
       .sort((a, b) => {
-        const value1 = a.amount * a.price;
-        const value2 = b.amount * b.price;
         // 按价值从高到低排序
-        return value2 - value1;
+        return b.value - a.value;
       })
       .map((e) => {
         // 格式化数据
         return {
-          name: e.name,
-          symbol: e.symbol,
-          logo: e.logo_url,
-          chain: chainsMap[e.chain]?.logo ?? chainsMap.eth.logo,
+          ...e,
           amount: formatNumber(e.amount),
-          value: formatNumber(e.amount * e.price),
+          value: formatNumber(e.value),
         };
       });
     return { total, tokens };
@@ -62,24 +87,49 @@ export const core = {
     const avatar = `https://cdn.stamp.fyi/avatar/eth:${address}?s=256`;
     return { ens, avatar };
   },
-  async getENSs(_address?: string) {
-    const address = _address ?? core.deafultAddress;
-    const data = {
-      query: `{ account(id:"${address.toLowerCase()}") { domains { name } } }`,
+  async getENSs(address?: string) {
+    const query = {
+      query: `
+        query getNames($id: ID!) {
+          account(id: $id) {
+            domains(first: 100, where: {name_ends_with: ".eth"}) {
+              name
+              registration {
+                registrationDate
+                expiryDate
+              }
+            }
+            wrappedDomains(first: 100, where: {name_ends_with: ".eth"}) {
+              domain {
+                name
+                registration {
+                  registrationDate
+                  expiryDate
+                }
+              }
+            }
+          }
+        }`,
+      variables: { id: address?.toLowerCase() },
+      operationName: 'getNames',
     };
-    const response = await http.post(
+    const response = await fetch(
       'https://api.thegraph.com/subgraphs/name/ensdomains/ens',
-      data,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(query),
+      },
     );
-    const domains = response?.data?.account?.domains ?? [];
-    return domains
-      .map((e) => e.name)
-      .sort((a, b) => {
-        return a.localeCompare(b, undefined, {
-          numeric: true,
-          sensitivity: 'base',
-        });
-      });
+    const result = await response.json();
+    return Array.from(
+      new Set([
+        ...result.data.account.domains.map((e: any) => e.name),
+        ...result.data.account.wrappedDomains.map((e: any) => e.domain.name),
+      ]),
+    ).filter((e: string) => e.endsWith('.eth'));
   },
   async getPOAP(_address?: string): Promise<POAP[]> {
     const address = _address ?? core.deafultAddress;
@@ -124,10 +174,9 @@ export const core = {
     option?: {
       callback?: (nfts: NFT[]) => void;
       max?: number;
-      rainbow?: boolean;
     },
   ): Promise<NFT[]> {
-    const { callback, max = 1000, rainbow = true } = option ?? {};
+    const { callback, max = 1000 } = option ?? {};
     const address = _address ?? core.deafultAddress;
 
     const _newIPFSClient = 'https://rss3.mypinata.cloud/ipfs/';
@@ -141,99 +190,86 @@ export const core = {
         : src;
     };
 
-    const getNFTs = async (pageToken = '') => {
-      const data = {
-        id: 1,
-        jsonrpc: '2.0',
-        method: 'ankr_getNFTsByOwner',
-        params: {
-          walletAddress: address,
-          pageSize: 50, // 上限50
-          pageToken, // 游标
-          blockchain: [
-            'eth',
-            'bsc',
-            'polygon',
-            'optimism',
-            'fantom',
-            'avalanche',
-            'arbitrum',
-          ],
+    const getNFTsMetamask = async (_next: any = undefined) => {
+      const datas = await http.proxy.get(
+        `https://nft.metafi.codefi.network/accounts/${address}/nfts?` +
+          (_next != undefined
+            ? _next.split('?')[1]
+            : 'mediaItems=true&limit=250'),
+        undefined,
+        {
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/116.0',
+            Origin: 'https://portfolio.metamask.io',
+            Referer: 'https://portfolio.metamask.io/',
+            'Cache-Control': 'no-cache',
+          },
         },
-      };
-      const datas = await http.post('https://rpc.ankr.com/multichain/', data);
-      let nfts = datas?.result?.assets ?? [];
+      );
+      let nfts = datas?.data ?? [];
       nfts = nfts.map((e) => {
         return {
-          name: e.name,
-          desp: e.description,
-          image: nftSrc(e.imageUrl),
-          chain: e.blockchain,
+          name: e.metadata?.name,
+          desp: e.metadata?.description,
+          image: nftSrc(
+            e.mediaItems?.high?.url
+              ? e.mediaItems?.high?.url
+              : e.metadata?.image
+              ? e.metadata?.image
+              : e.metadata?.image_url
+              ? e.metadata?.image_url
+              : e.image
+              ? e.image
+              : e.tokenUrl,
+          ),
+          chain: e.chainId,
           // details
+          isErc721: e.isErc721,
+          isPossibleSpam: e.isPossibleSpam,
           tokenId: e.tokenId,
-          contractAddress: e.contractAddress,
-          contractType: e.contractType,
-          collectionName: e.collectionName,
+          contractAddress: e.tokenAddress,
+          collectionName: e.name,
         };
       });
+      const blackContracts = [
+        '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85',
+        '0xe5193303ae1e909805f16f0fc19f1e23992d2351',
+        '0x465ea4967479a96d4490d575b5a6cc2b4a4bee65',
+        '0x9d13e3561c51ca3a5c5308e8528df7afd8de6485',
+        '0x53a0018f919bde9c254bda697966c5f448ffddcb',
+        '0x10d5e4da056ae64d0ed702698b6d8544a050b4eb',
+        '0x90916ee6694dfe9c2979c8b05454d6db4612e5e3',
+        '0xdfd3882f95c7109423b89ed9bac6f5800a6c2cf5',
+        '0x53a0018f919bde9c254bda697966c5f448ffddcb',
+        '0x26b8d77d189b5e4fbd95d695690576a544590b25',
+        '0xe98d9f7b48a5c6d31fbe8a5f343cd3e96357f6ea',
+      ];
       nfts = nfts.filter((e) => {
         return (
           isNotEmpty(e.name) &&
           isNotEmpty(e.image) &&
-          (e.contractType.includes('721') || e.contractType.includes('1155'))
+          e.isErc721 &&
+          !e.isPossibleSpam &&
+          !blackContracts.includes(e.contractAddress)
         );
       });
-      const nextPageToken = datas?.result?.nextPageToken;
+      const nextPage = datas?.nextPage;
       return {
         nfts,
-        nextPageToken,
-      };
-    };
-
-    const getNFTsRainbow = async (next = 'start') => {
-      const datas = await http.proxy.get('https://rainbow.me/api/assets', {
-        address: address,
-        cursor: next,
-      });
-      let nfts = datas?.results ?? [];
-      nfts = nfts.map((e) => {
-        return {
-          name: e.metadata.name,
-          desp: e.metadata.description,
-          image: nftSrc(e.metadata.image_url),
-          chain: e.asset_contract.chain_identifier,
-          // details
-          tokenId: e.token_id,
-          contractAddress: e.asset_contract.address,
-          contractType: e.asset_contract.contract_standard,
-          collectionName: e.collection.name,
-        };
-      });
-      nfts = nfts.filter((e) => {
-        return (
-          isNotEmpty(e.name) &&
-          isNotEmpty(e.image) &&
-          (e.contractType.includes('721') || e.contractType.includes('1155'))
-        );
-      });
-      const nextPageToken = datas?.next;
-      return {
-        nfts,
-        nextPageToken,
+        nextPage,
       };
     };
 
     let finalNFTs: NFT[] = [];
-    let nextPageToken;
+    let nextPage;
     // eslint-disable-next-line no-constant-condition
     while (true) {
-      const result = rainbow
-        ? await getNFTsRainbow(nextPageToken)
-        : await getNFTs(nextPageToken);
+      const result = await getNFTsMetamask(nextPage);
       finalNFTs = [...finalNFTs, ...result.nfts];
       callback?.(finalNFTs);
-      nextPageToken = result.nextPageToken;
-      if (finalNFTs.length >= max || isEmpty(result.nextPageToken)) {
+      nextPage = result.nextPage;
+      if (finalNFTs.length >= max || isEmpty(result.nextPage)) {
         break;
       }
     }
